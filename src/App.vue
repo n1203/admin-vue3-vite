ed971f9d056df9f61a1f78c0c26bd23b5920238b
wushuang <wushuang@gaoding.com>
2022-01-15 14:02:56 +0800

    feat: 第一次

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Main from '@/layout/main.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
+import { createPinia } from 'pinia'
+// 引入组件库全局样式资源
+import 'tdesign-vue-next/es/style/index.css';
+import './styles'
+
+createApp(App).use(createPinia()).use(TDesign).mount('#app')
