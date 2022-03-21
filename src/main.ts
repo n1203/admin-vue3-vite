c579c8ed62c71863746732a10bcb6cd6d26fa716
wushuang <wushuang@gaoding.com>
2022-03-21 18:22:15 +0800

    新增：初始化云开发环境

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ import { createPinia } from 'pinia'
 // 引入组件库全局样式资源
 import 'tdesign-vue-next/es/style/index.css';
 import './styles'
+import './database'
+
 
 createApp(App).use(createPinia()).use(TDesign).mount('#app')
